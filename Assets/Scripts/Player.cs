19d791c6246f56e3cfa0364f136b52e791042d87
yonty <Yontymanor@gmail.com>
2018-03-19 00:13:37 +0200

    Finished part one of project, game now works smoothly. Trying to adjust for android

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,78 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour {
 
     public float tapForce = 12f;
     public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public Color red;
+    public Color blue;
+    public Color green;
+    public Color yellow;
+    private String currColor;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody2D>();
-
+        sr = GetComponent<SpriteRenderer>();
+        SetRandomColor();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void SetRandomColor()
+    {
+        String current = currColor;
+        int color = Random.Range(0, 4);
+        
+        switch (color) {
+            case 0:
+                currColor = "Red";
+                sr.color = red;
+                break;
+            case 1:
+                currColor = "Blue";
+                sr.color = blue;
+
+                break;
+            case 2:
+                currColor = "Green";
+                sr.color = green;
+
+                break;
+            case 3:
+                currColor = "Yellow";
+                sr.color = yellow;
+
+                break;
+        }
+        if (current == currColor)
+        {
+            SetRandomColor();
+        }
+        else return;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	    if(Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump")) {
             rb.velocity = Vector2.up * tapForce;
         }	
 	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+           
+        if (other.tag == "ColorChanger")
+        {
+            SetRandomColor();
+            Destroy(other.gameObject);
+            return; 
+        }
+        if (currColor != other.tag) {
+            Debug.Log(other.tag);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+
+    }
 }
---
+++ Assets/Scripts/RotateCircle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateCircle : MonoBehaviour {
+    public float rotateSpeed = 100f;
+    
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);		
+	}
+
+ 
+}
