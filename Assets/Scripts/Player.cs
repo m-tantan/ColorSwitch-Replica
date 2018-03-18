05ac4a2b6272ac6580732269b1b3361df032e68f
yonty <Yontymanor@gmail.com>
2018-03-18 17:48:18 +0200

    Can now tap jump!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float tapForce = 10f;
+    public Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0) || Input.GetButtonDown("Jump")) {
+            rb.velocity = Vector2.up * tapForce;
+        }	
+	}
+}
