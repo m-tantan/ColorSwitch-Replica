0eb96b1a780f0fc4fbbf8d1cf8bf693c7e89cb38
yonty <Yontymanor@gmail.com>
2018-03-20 19:27:10 +0200

    Version 2.00 Release Notes: Added Menu Added Options Menu Music, Sounds, Level still don't work Gameplay same as version 1.02

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	
+	public void SwitchScene(int scene) {
+        
+        Debug.Log("Clicked Play");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+    }
+
+    public void OptionsMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void FromOptionsToMenu() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -2);
+    }
+
+    public void ExitScene()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
